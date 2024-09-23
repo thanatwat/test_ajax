599423ee7bed037c91e551f9daab36fb2b963f0b
thanatwat <tnutvuss@gmail.com>
2024-09-23 20:39:43 +0700

    update sample code

--- test_ajax.php
+++ test_ajax.php
@@ -1,3 +1,23 @@
 <?php
 include("conn.php");
 include("query.php");
+
+
+$status = $_POST["status"];
+
+if($status=="show_list"){
+    //do query
+     $select = "SELECT someting FROM sometable WHERE somewhere ";
+     $row = getDetail($select, 1);
+     if (!empty($row)) {
+       foreach ($row as $item) {
+            //output
+        }
+     }
+}
+
+if($status=="addnew"){
+    //do add new
+    $insert = "";
+    Update($insert, 1);
+}
